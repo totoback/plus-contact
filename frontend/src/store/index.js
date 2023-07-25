9b684b12ba26fc8d144d12227d59decae4e7ee14
지영 <toto.joung@gmail.com>
2023-07-25 22:32:25 +0900

    230725

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,8 +1,34 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore, persistStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
+import storage from "redux-persist/lib/storage";
+import {
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+const rootReducer = combineReducers({
+  user: userReducer,
+  post: postReducer,
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
